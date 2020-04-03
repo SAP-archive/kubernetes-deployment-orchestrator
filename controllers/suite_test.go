36c91503ffa5ff88b06831076e1d0dc51d77952b
Ulrich Kramer <ulrich.kramer@web.de>
2020-04-03 17:22:55 +0200

    Update to newest versions

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -24,6 +24,7 @@ import (
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -39,5 +40,5 @@ func TestAPIs(t *testing.T) {
 
 	RunSpecsWithDefaultAndCustomReporters(t,
 		"Controller Suite",
-		[]Reporter{envtest.NewlineReporter{}})
+		[]Reporter{printer.NewlineReporter{}})
 }
