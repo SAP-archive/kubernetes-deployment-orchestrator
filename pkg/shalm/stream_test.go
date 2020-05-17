ae4c330d561c2c74cbfbc0d968d3fbacb8f38ed7
wonderix <wonderix@googlemail.com>
2020-05-17 10:16:16 +0200

    Increase test coverage

---
+++ pkg/shalm/stream_test.go
@@ -0,0 +1,38 @@
+package shalm
+
+import (
+	"errors"
+	"io"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Stream", func() {
+
+	Context("stream", func() {
+		It("behaves like starlark value", func() {
+			s := &stream{Stream: func(w io.Writer) error { _, err := w.Write([]byte("test")); return err }}
+			Expect(s.String()).To(ContainSubstring("test"))
+			Expect(s.Type()).To(Equal("stream"))
+			Expect(func() { s.Hash() }).Should(Panic())
+			Expect(s.Truth()).To(BeEquivalentTo(false))
+			_, err := s.Attr("test")
+			Expect(err).To(HaveOccurred())
+			Expect(s.AttrNames()).To(BeEmpty())
+		})
+	})
+	Context("ErrorStream", func() {
+		It("fails", func() {
+			s := ErrorStream(errors.New("test"))
+			Expect(s(nil)).To(MatchError("test"))
+		})
+	})
+	Context("ObjectErrorStream", func() {
+		It("fails", func() {
+			s := ObjectErrorStream(errors.New("test"))
+			Expect(s(nil)).To(MatchError("test"))
+		})
+	})
+
+})
